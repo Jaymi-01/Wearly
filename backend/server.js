75f88cedb61bfacdfddebcf790ae2340286d9270
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-08-19 00:49:41 +0100

    connected the database

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoute.js";
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // Middleware to parse JSON bodies
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+
+  connectDB();
+});
+
