8f96ee14aa0df4be3f1da6dfa5d61404785c5507
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-08-19 22:50:20 +0100

    imported get featured

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,9 +1,12 @@
-import {express} from "express";
+import express from "express";
+import {getAllProducts, getFeaturedProducts} from "../controllers/productController.js";
+import {protectRoute, adminRoute} from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
+router.get("/featured", getFeaturedProducts);
 
 export default router;
