64550376e5291ceaf2421a660340ed514a748eeb
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-08-19 22:47:01 +0100

    added get featured product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,4 @@
-import Product from "../models/productModel";
+import Product from "../models/productModel.js";
 
 export const getAllProducts = async (req, res) => {
   try {
@@ -9,3 +9,25 @@ export const getAllProducts = async (req, res) => {
     res.status(500).json({ message: "Server error", error: error.message });
   }
 };
+
+export const getFeaturedProducts = async (req, res) => {
+  try {
+    let featuredProducts = await redis.get("featured_products")
+    if (featuredProducts) {
+      return res.json({ products: JSON.parse(featuredProducts) });
+    }
+    //fetch from mongoDb
+    //.lean() returns a plain js obj instead of mongoDB obj
+    featuredProducts = await Product.find({ isFeatured: true }).lean();
+    if (!featuredProducts) {
+      return res.status(404).json({ message: "No featured products found" });
+    }
+
+    //store in redis
+    await redis.set("featured_products", JSON.stringify(featuredProducts));
+    res.json({ products: featuredProducts });
+  } catch (error) {
+    console.error("Featured products fetching controller error:", error.message);
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+}
