cbcc9929f56d6bc2e9c1272a52a1f9a8aed65366
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-08-19 20:47:19 +0100

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authRoute.js";
+import productRoutes from "./routes/productRoute.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -13,6 +14,7 @@ app.use(express.json()); // Middleware to parse JSON bodies
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
