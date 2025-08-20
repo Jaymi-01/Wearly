467304185a400b821e36593f0a57d7e75162982c
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-08-20 23:15:45 +0100

    added updateQuantity function

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -1,44 +1,62 @@
-export const getCartProducts = async (req, res) => {
-
-}
+export const getCartProducts = async (req, res) => {};
 
 export const addToCart = async (req, res) => {
-    try {
-        const { productId, quantity } = req.body;
-        const userId = req.user;
+  try {
+    const { productId, quantity } = req.body;
+    const userId = req.user;
 
-        const existingItem = user.cartItems.find(item => item.id === productId);
-        if (existingItem) {
-            existingItem.quantity += 1;
-        }else{
-            user.cartItems.push( productId);
-        }
-
-        await user.save();
-        res.status(201).json({ message: "Product added to cart" });
-    } catch (error) {
-        console.log("Add to cart controller error:", error.message);
-        res.status(500).json({ message: "Error adding product to cart" });
+    const existingItem = user.cartItems.find((item) => item.id === productId);
+    if (existingItem) {
+      existingItem.quantity += 1;
+    } else {
+      user.cartItems.push(productId);
     }
-}
+
+    await user.save();
+    res.status(201).json({ message: "Product added to cart" });
+  } catch (error) {
+    console.log("Add to cart controller error:", error.message);
+    res.status(500).json({ message: "Error adding product to cart" });
+  }
+};
 
 export const removeAllFromCart = async (req, res) => {
-    try {
-        const {productId} = req.body;
-        const user = req.user;
-        if (!productId){
-            user.cartItems = [];
-        }else{
-            user.cartItems = user.cartItems.filter((item) => item.id !== productId);
-        }
-        await user.save();
-        res.json(user.cartItems);
-    } catch (error) {
-        console.log("Remove all from cart controller error:", error.message);
-        res.status(500).json({ message: "Error removing items from cart" });
+  try {
+    const { productId } = req.body;
+    const user = req.user;
+    if (!productId) {
+      user.cartItems = [];
+    } else {
+      user.cartItems = user.cartItems.filter((item) => item.id !== productId);
     }
-}
+    await user.save();
+    res.json(user.cartItems);
+  } catch (error) {
+    console.log("Remove all from cart controller error:", error.message);
+    res.status(500).json({ message: "Error removing items from cart" });
+  }
+};
 
 export const updateQuantity = async (req, res) => {
-
-}
+  try {
+    const { id: productId } = req.params;
+    const { quantity } = req.body;
+    const user = req.user;
+    const existingItem = user.cartItems.find((item) => item.id === productId);
+    if (existingItem) {
+      if (quantity === 0) {
+        user.cartItems = user.cartItems.filter((item) => item.id !== productId);
+        await user.save();
+        return res.json(user.cartItems);
+      }
+      existingItem.quantity = quantity;
+      await user.save();
+      res.json(user.cartItems);
+    } else {
+      return res.status(404).json({ message: "Item not found in cart" });
+    }
+  } catch (error) {
+    console.log("Update quantity controller error:", error.message);
+    res.status(500).json({ message: "Error updating item quantity" });
+  }
+};
