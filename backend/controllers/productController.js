173180873c0074b3e4d87fad6b05d4f4d7a75299
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-08-20 22:07:07 +0100

    created toggleFeaturedProduct function

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -122,3 +122,28 @@ export const getProductsByCategory = async (req, res) => {
     res.status(500).json({ message: "Server error", error: error.message });
   }
 };
+
+export const toggleFeaturedProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (product){
+      product.isFeatured = !product.isFeatured;
+      const updatedProduct = await product.save();
+      await updateFeaturedProductsCache();
+      res.json({ message: "Product featured status updated", product: updatedProduct });
+    }else{
+      res.status(404).json({ message: "Product not found" });
+    }
+  } catch (error) {
+    console.error("Toggle featured product controller error:", error.message);
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+}
+async function updateFeaturedProductsCache() {
+  try{
+    const featuredProducts = await Product.find({ isFeatured: true }).lean();
+    await redis.set("featured_products", JSON.stringify(featuredProducts),);
+  }catch (error) {
+    console.error("Error updating featured products cache:", error.message);
+  }
+}
