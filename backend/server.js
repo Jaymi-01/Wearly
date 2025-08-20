9857c17ff33497c7dd331f022d45a4cccfe26e6c
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-08-20 23:54:44 +0100

    Adds coupons route to the backend

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authRoute.js";
 import productRoutes from "./routes/productRoute.js";
 import cartRoutes from "./routes/cartRoute.js";
+import couponsRoutes from "./routes/couponsRoute.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -17,6 +18,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponsRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
